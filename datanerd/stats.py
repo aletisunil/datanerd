e83f9f3fadfffe89ac6deeec872738ed285d03c8
aletisunil <aletisunil1997@gmail.com>
2024-06-16 15:54:10 -0500

    removed non-zero stats column

--- datanerd/stats.py
+++ datanerd/stats.py
@@ -2,21 +2,9 @@ import pandas as pd
 import numpy as np
 
 def stats(df):
-    
     df_stats=pd.DataFrame()
-
     df_stats=df.describe(percentiles=[.1,.2,.25,.3,.4,.5,.6,.7,.75,.8,.9,.95,.99]).T
-
     dfMissing=((df.isna().sum()/len(df))*100).to_frame('%ofMissingValues')
-
-    num_columns = df.select_dtypes(exclude=['object', 'datetime64[ns]','timedelta64[ns]']).columns
-    cat_columns = df.select_dtypes(['object']).columns
-
-    dfNonZero=df[num_columns].apply(lambda x: 100 * ((x > 0) & (x.notnull())).mean()).to_frame("%ofNonZeroValues")
-
-    dfUnique=df.apply(lambda x: x.nunique(dropna=False), axis=0).to_frame("#ofUniqueValues")
-
-
-    stats=pd.concat([df_stats,dfMissing,dfNonZero,dfUnique],ignore_index=False,axis=1)
-
+    dfUnique=df.apply(lambda x: x.nunique(dropna=True), axis=0).to_frame("#ofUniqueValues")
+    stats=pd.concat([df_stats,dfMissing,dfUnique],ignore_index=False,axis=1)
     return stats
