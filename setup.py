0ab6efb54e224222891ebeb6e259f54d3dedbe58
aletisunil <aletisunil1997@gmail.com>
2024-06-28 23:26:11 -0500

    Version 1.0

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.7'
-DESCRIPTION = 'Contains multiple functions, stats() and iv_woe(), pushdb()'
-#LONG_DESCRIPTION = 'stats() function takes the dataframe and returns statistics i.e count, percentiles, unique_values etc and iv_woe() function is used to calculate the Weight of Evidence (woe) and Information Value (iv) for a dataframe'
+VERSION = '1.0'
+DESCRIPTION = 'Contains multiple functions stats(), iv_woe(), pushdb(), teams_webhook(), and ntfy()'
+
 
 # Setting up
 setup(
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'sqlalchemy', 'pyodbc'],
+    install_requires=['pandas', 'numpy', 'sqlalchemy', 'pyodbc', 'requests'],
     keywords=['python', 'describe', 'stats', 'unique values', 'information value', 'woe','iv'],
     classifiers=[
         "Programming Language :: Python :: 3",
