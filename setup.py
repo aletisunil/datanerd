ede34cebc7a253f6ba19feffd9960bc84d7dad79
aletisunil <aletisunil1997@gmail.com>
2024-12-17 22:07:24 -0600

    Resolve uniques values bug

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0'
+VERSION = '1.2'
 DESCRIPTION = 'Contains multiple functions stats(), iv_woe(), pushdb(), teams_webhook(), and ntfy()'
 
 
